d5543575d9609e2bf957a6a4571f50fc6e517ad4
Ana Baez <ana.baez@bipb.com>
2015-04-01 10:20:12 +0100

    renamed import folders for clarity

---
+++ feed_import/ftp_connection.py
@@ -0,0 +1,31 @@
+"""A test file investigating how to use ftp
+commands to pull from the server"""
+
+from ftplib import FTP
+import sys
+import StringIO
+
+try:
+    #pull files from the ftp server
+    ftp_url = 'ftp.euromoneydigital.com'
+    user = 'investec'
+    password = 'Jc3L9mn3'
+    file_identifier = 'News'
+
+    ftp = FTP(ftp_url)     # connect to host, default port
+    ftp.login(user, password)
+    #print ftp.
+    print ftp.retrlines('LIST')
+    for item in ftp.nlst():
+        if item.find(file_identifier) != -1:
+    		print 'Found'
+    		main_file = item
+    		print main_file
+    		print 'RETR %s' % main_file
+    		buf = StringIO.StringIO()
+    		print ftp.retrbinary('RETR %s' % main_file, buf.write)
+    		print len(buf.getvalue())
+    		buf.seek(0)
+    		print buf.seek(0) 
+except Exception as e :
+	print 'failed', e
